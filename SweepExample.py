4e4860cfc8122e539b03ac187ec4413f8174c380
nskazmi <142994595+nskazmi@users.noreply.github.com>
2023-12-20 12:17:48 +0100

    added metrics

--- SweepExample.py
+++ SweepExample.py
@@ -1,29 +1,30 @@
-## To do
-### 1. Clean sweep_config
-### 2. Run on all models
-### 3. Try new sweep values if results are not satisfactory
-### 4. Find a way to store transformed datasets
+# To do
+# 1. Clean sweep_config
+# 2. Run on all models
+# 3. Try new sweep values if results are not satisfactory
+# 4. Find a way to store transformed datasets
 
+from utils import fetch_data, transform_data, get_config_path, get_config_from_path, retrain_transformed_sweep, evaluate
+from views_forecasts.extensions import *
+from pathlib import Path
+import argparse
+import copy
+import wandb
 import numpy as np
 import warnings
 warnings.filterwarnings("ignore")
-import wandb
-import copy
-import argparse
-from pathlib import Path
-
-from views_forecasts.extensions import *
-from utils import fetch_data, transform_data, get_config_path, get_config_from_path, retrain_transformed_sweep, evaluate
 
 
 PARA_DICT = {
-    'rf': ['transform', 'n_estimators', 'n_jobs', 'learning_rate', 'max_depth', 'min_child_weight', 'subsample', 'colsample_bytree'],
-    'xgb': ['transform', 'n_estimators', 'n_jobs', 'learning_rate', 'max_depth', 'min_child_weight', 'subsample', 'colsample_bytree'],
+    'rf': ['transform', 'n_estimators', 'n_jobs', 'learning_rate'],
+    'xgb': ['transform', 'n_estimators', 'n_jobs', 'learning_rate'],
     'gbm': ['transform', 'n_estimators', 'n_jobs', 'learning_rate', 'max_depth', 'min_samples_split', 'min_samples_leaf']
 }
 
+
 def train():
-    run = wandb.init(config=common_config, project=wandb_config['project'], entity=wandb_config['entity'])
+    run = wandb.init(config=common_config,
+                     project=wandb_config['project'], entity=wandb_config['entity'])
     wandb.config.update(model_config, allow_val_change=True)
 
     run_name = ''
@@ -39,8 +40,10 @@ def train():
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Method for sweeping on W&B')
-    parser.add_argument('-l', metavar='level', type=str, required=True, choices=['cm', 'pgm'])
-    parser.add_argument('-c', metavar='config', type=str, required=True, help='Path to the configuration directory')
+    parser.add_argument('-l', metavar='level', type=str,
+                        required=True, choices=['cm', 'pgm'])
+    parser.add_argument('-c', metavar='config', type=str,
+                        required=True, help='Path to the configuration directory')
     args = parser.parse_args()
 
     level = args.l
@@ -51,9 +54,11 @@ if __name__ == '__main__':
     para_transformed = {}
     qslist, Datasets = fetch_data(level)
     for t in transforms:
-        Datasets_transformed[t], para_transformed[t] = transform_data(Datasets, t, by_group=True)
+        Datasets_transformed[t], para_transformed[t] = transform_data(
+            Datasets, t, by_group=True)
 
-    common_config_path, wandb_config_path, model_config_path, sweep_config_path = get_config_path(config_path)
+    common_config_path, wandb_config_path, model_config_path, sweep_config_path = get_config_path(
+        config_path)
     common_config = get_config_from_path(common_config_path, 'common')
     wandb_config = get_config_from_path(wandb_config_path, 'wandb')
 
@@ -61,7 +66,8 @@ if __name__ == '__main__':
         if sweep_file.is_file():
             model_file = model_config_path / sweep_file.name
             if not model_file.is_file():
-                raise FileNotFoundError(f'The corresponding model configuration file {model_file} does not exist.')
+                raise FileNotFoundError(
+                    f'The corresponding model configuration file {model_file} does not exist.')
 
             sweep_config = get_config_from_path(sweep_file, 'sweep')
             model_config = get_config_from_path(model_file, 'model')
